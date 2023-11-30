39b6bdcaf9d3455baeb8c121f257c73c3611b1f0
webmasterMcc <webmaster@marbleceramiccorp.com.au>
2023-11-30 17:13:14 +1100

    first commit

---
+++ src/App2.jsx
@@ -0,0 +1,40 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+// const Home = () => <div>Home Page</div>;
+// const About = () => <div>About Page</div>;
+
+
+const App2 = () => {
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      </Router>
+    );
+  };
+
+
+  export default App2;
---
+++ src/Contact.jsx
@@ -0,0 +1,12 @@
+
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Reach out to us!</p>
+    </div>
+  );
+};
+
+export default Contact;
